2b062708ddf8b1d7be223d5c63d9f33febf3d6ff
Nacho <polettiignacio7@gmail.com>
2025-05-29 13:43:36 -0300

    FIX | Deleted node modules

--- scraper.js
+++ scraper.js
@@ -1,4 +1,4 @@
-const { chromium } = require('playwright'); 
+const { chromium } = require('playwright-chromium');
 
 async function scrapeRemax(pageNumber = 0, endPage) {
     let allProperties = [];
